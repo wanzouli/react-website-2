934cc7c010ee41178f3f8d5b4b46c76013e6a137
Rawan <example@gmail.com>
2024-05-02 14:49:34 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
+
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
