96a605cc713d9872195f165f1e2cf535953bafc7
Rawan <example@gmail.com>
2024-05-02 14:26:44 -0700

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Work from './pages/Work';
 import About from './pages/About';
 import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import {Home, About, Work} from './pages'
+// import {Home, About, Work} from './pages'
 
 
 function App() {
