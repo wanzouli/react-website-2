95421a53f49cafd9ed312236abb752ea658b85d7
Rawan <example@gmail.com>
2024-05-02 15:14:01 -0700

    adding a button

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -10,7 +10,10 @@ export default function Work() {
         <div>
             <h1>Work Page</h1>
             <p>This is the Work page content.</p>
-            <button onClick={handleClick}>Click me</button>
+            {/* <button onClick={handleClick}>Click me</button> */}
+            <a href="https://www.linkedin.com/in/rawanghazouli/" target="_blank" rel="noopener noreferrer">
+                Visit my LinkedIn profile
+            </a>
         </div>
     );
 }  
